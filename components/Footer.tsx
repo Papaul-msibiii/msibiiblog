6f2d9444c15e674ace1b769e813a279f66a27a40
Paul Gomis <gomispapaul6@gmail.com>
2024-06-23 22:36:35 +0000

    social links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,6 @@
 import { DiscordLogoIcon, GitHubLogoIcon, LinkedInLogoIcon } from "@radix-ui/react-icons";
+import { Target } from "lucide-react";
+import Link from "next/link";
 import React from "react";
 
 export default function Footer() {
@@ -15,9 +17,13 @@ export default function Footer() {
             </p>
           </div>
           <div className="flex items-center gap-2">
-            <GitHubLogoIcon className="w-5 h-5" />
-            <LinkedInLogoIcon className="w-5 h-5" />
-            <DiscordLogoIcon className="w-5 h-5" />
+            <Link target="_blank" href={"https://github.com/Papaul-msibiii/"}>
+                <GitHubLogoIcon className="w-5 h-5" />
+            </Link>
+            <Link target="_blank" href={"https://www.linkedin.com/in/paul-gomis/"}>
+                <LinkedInLogoIcon className="w-5 h-5" />
+            </Link>
+            {/* <DiscordLogoIcon className="w-5 h-5" /> */}
           </div>
         </div>
         <h1>
